bbd272f39a8555eddd5f09efe8680ca326572fdc
Undanganonlinejpg <pongsep87@gmail.com>
2025-01-26 23:24:00 +0700

    Update server.js

--- scripts/server.js
+++ scripts/server.js
@@ -14,7 +14,11 @@ console.log("Isi db:", db);
 
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: ["https://undangan-muamar-ira.vercel.app"],
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+}));
 app.use(express.json());app.use("/api", require("./routes/api"));
 app.use(express.static(path.join(__dirname, "../")));
 
