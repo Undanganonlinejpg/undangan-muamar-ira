f388cea24e4dc70d69960d9b48033d1587fdd9ff
Undanganonlinejpg <pongsep87@gmail.com>
2025-01-26 23:13:41 +0700

    server.js

--- scripts/server.js
+++ scripts/server.js
@@ -15,9 +15,7 @@ console.log("Isi db:", db);
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.json());
-
-
+app.use(express.json());app.use("/api", require("./routes/api"));
 app.use(express.static(path.join(__dirname, "../")));
 
 app.post("/wishes", (req, res) => {
